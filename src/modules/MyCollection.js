06181dca4a4667dc2e113ff55401a23fda6aa443
Jed Duffey <jedduffey@gmail.com>
2023-01-26 19:52:36 -0500

    Implement core functionality of module

--- src/modules/MyCollection.js
+++ src/modules/MyCollection.js
@@ -1,10 +1,84 @@
+var tables = {};
+function nCr(n, r) {
+    var key = n + "," + r;
+    if (key in tables)
+        return tables[key];
+
+    if (n < r)
+        return 0;
+
+    if (r == 0)
+        return 1;
+
+    if (r == 1)
+        return n;
+
+    if (n == 1)
+        return 1;
+
+    var val = nCr(n - 1, r - 1) + nCr(n - 1, r);
+    tables[key] = val;
+
+    return val;
+}
+
+function findUniqueCombination(n, k, m) {
+    var S = [];
+    for (var j = 0; j < k; j++)
+        S.push(j);
+
+    var i = 0;
+    var t = 0;
+
+    while (i < k) {
+        var v = nCr(n - 1 - t, k - i - 1);
+        if (v <= m) {
+            m = m - v;
+        } else {
+            S[i] = t;
+            i = i + 1;
+        }
+
+        t = t + 1;
+    }
+
+    return S;
+}
+
+const HASH_RANGE_SIZE = BigInt("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF") + 1n;
+
+function getLexicoOrderIndex(numCombos, hashAsNumber) {
+    const chunkSize = HASH_RANGE_SIZE / BigInt(numCombos);
+    const myBin = hashAsNumber / chunkSize;
+
+    const result = Number(myBin);
+
+    return result;
+}
+
 export class MyCollection {
     constructor(collection) {
         this.collection = collection;
+        this.collectionSize = collection.length;
     }
 
     getCombination(groupSize, hash) {
-        console.log("groupSize:", groupSize, "hash:", hash);
-        return this.collection;
+        const numCombos = nCr(this.collectionSize, groupSize);
+
+        const hashAsNumber = BigInt("0x" + hash);
+
+        const lexicoOrderIndex = getLexicoOrderIndex(numCombos, hashAsNumber);
+
+        const uniqueCombinationIndices = findUniqueCombination(
+            this.collectionSize,
+            groupSize,
+            lexicoOrderIndex,
+        );
+
+        const uniqueCombinationElements = uniqueCombinationIndices.map((index) => {
+            return this.collection[index];
+        });
+
+        return uniqueCombinationElements;
     }
 }
--- src/test/MyCollection.test.js
+++ src/test/MyCollection.test.js
@@ -1,24 +1,97 @@
 import { MyCollection } from "../modules/MyCollection";
 
 describe("MyCollection.getCombination given hash returns combination", () => {
+    const GROUP_SIZE_ONE = 1;
+    const GROUP_SIZE_TWO = 2;
 
     describe("for collection with 1 element(s)", () => {
-        const collection = ["a"];
-        describe("for group size 1", () => {
-            const groupSize = 1;
+        const collection = [0];
+
+        describe(`group size ${GROUP_SIZE_ONE} for collection [${collection}]`, () => {
             it.each([
                 [
                     "0000000000000000000000000000000000000000000000000000000000000000",
-                    ["a"],
+                    [0],
                 ],
                 [
                     "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
-                    ["a"],
+                    [0],
                 ],
-            ])("for hash = %s", (hash, expected) => {
+            ])("%s -> %s", (hash, expected) => {
                 const sut = new MyCollection(collection);
 
-                const actual = sut.getCombination(groupSize, hash);
+                const actual = sut.getCombination(GROUP_SIZE_ONE, hash);
+
+                expect(actual).toEqual(expected);
+            });
+        });
+    });
+
+    describe("for collection with 2 element(s)", () => {
+        const collection = [0, 1];
+
+        describe(`group size ${GROUP_SIZE_ONE} for collection [${collection}]`, () => {
+            it.each([
+                [
+                    "0000000000000000000000000000000000000000000000000000000000000000",
+                    [0],
+                ],
+                [
+                    "0000000000000000000000000000000000000000000000000000000000000001",
+                    [0],
+                ],
+                [
+                    "7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                    [0],
+                ],
+                [
+                    "8000000000000000000000000000000000000000000000000000000000000000",
+                    [1],
+                ],
+                [
+                    "8000000000000000000000000000000000000000000000000000000000000001",
+                    [1],
+                ],
+                [
+                    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                    [1],
+                ],
+            ])("%s -> %s", (hash, expected) => {
+                const sut = new MyCollection(collection);
+
+                const actual = sut.getCombination(GROUP_SIZE_ONE, hash);
+
+                expect(actual).toEqual(expected);
+            });
+        });
+
+        describe(`group size ${GROUP_SIZE_TWO} for collection [${collection}]`, () => {
+            it.each([
+                [
+                    "0000000000000000000000000000000000000000000000000000000000000000",
+                    [0, 1],
+                ],
+
+                [
+                    "0000000000000000000000000000000000000000000000000000000000000001",
+                    [0, 1],
+                ],
+                [
+                    "7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                    [0, 1],
+                ],
+                [
+                    "8000000000000000000000000000000000000000000000000000000000000000",
+                    [0, 1],
+                ],
+                [
+                    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                    [0, 1],
+                ],
+            ])("%s -> %s", (hash, expected) => {
+                const sut = new MyCollection(collection);
+
+                const actual = sut.getCombination(GROUP_SIZE_TWO, hash);
 
                 expect(actual).toEqual(expected);
             });
